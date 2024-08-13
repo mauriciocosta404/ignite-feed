508dd4316e05e78c499d6531f509ddb809a2eeb6
mauriciocosta404 <mc611641@gmail.com>
2024-08-13 18:55:44 +0100

    feat: adicionando o post

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./index.module.css";
 
 interface PostPros {
   author: string;
@@ -7,9 +8,29 @@ interface PostPros {
 
 export const Post: React.FC<PostPros> = ({ author, content }) => {
   return (
-    <>
-      <strong>{author}</strong>
-      <p>{content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/95001318?v=4"/>
+          
+          <div className={styles.authorInfo}>
+            <strong>Mauricio Costa</strong>
+            <span>Web Developer</span>
+          </div>
+
+          <time title="11 de Maio as 8:13" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+        </div>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉 <a href="">jane.design/doctorcare</a></p>
+
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
   );
 };
